154f385bf34ad717bd8f535e196d58e6700dbe6e
Mazyaa <muhamadazisudarya@unbaja.ac.id>
2025-07-06 17:09:51 +0700

    fix(docs): fixing docs user activation

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.post('/auth/register', authController.register);
 router.post('/auth/login', authController.login);
-router.post('auth/activation', authController.activation);
+router.post('/auth/activation', authController.activation);
 router.get('/auth/me',
     authMiddleware,
     authController.me
