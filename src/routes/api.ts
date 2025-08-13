48d3a6a4ad0403d858768b4748557de116f2c400
Mazyaa <muhamadazisudarya@unbaja.ac.id>
2025-08-13 13:41:37 +0700

    feat: have been finished cretaed acl middleware

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,24 +1,25 @@
-import express from 'express';
-import * as authController from '../controllers/authContoller';
-import authMiddleware from '../middlewares/authMiddleware';
-import aclMiddleware from '../middlewares/acl.middleware';
-import { ROLES } from '../utils/constant';
+import express, { Request, Response } from "express";
+import * as authController from "../controllers/authContoller";
+import authMiddleware from "../middlewares/authMiddleware";
+import aclMiddleware from "../middlewares/acl.middleware";
+import { ROLES } from "../utils/constant";
 
 const router = express.Router();
 
-router.post('/auth/register', authController.register);
-router.post('/auth/activation', authController.activation);
-router.post('/auth/login', authController.login);
-router.get('/auth/me',
-    authMiddleware,
-    authController.me
-);
+router.post("/auth/register", authController.register);
+router.post("/auth/activation", authController.activation);
+router.post("/auth/login", authController.login);
+router.get("/auth/me", authMiddleware, authController.me);
 
-router.get('/test-acl', aclMiddleware([ROLES.ADMIN, ROLES.MEMBER]), (req, res) => {
+router.get(
+  "/test-acl",
+  [authMiddleware, aclMiddleware([ROLES.ADMIN, ROLES.MEMBER])],
+  (_req: Request, res: Response) => {
     res.status(200).json({
-        data: "success",
-        message: "OK"
+      data: "success",
+      message: "OK",
     });
-})
+  }
+);
 
 export default router;
