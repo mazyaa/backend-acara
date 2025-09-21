74d043b83ece9dfef0e620e26a66402bf9d756c8
Mazyaa <muhamadazisudarya@unbaja.ac.id>
2025-09-21 23:41:15 +0700

    feat: have been created shema for event

--- src/models/eventModel.ts
+++ src/models/eventModel.ts
@@ -20,9 +20,83 @@ export const eventDAO = Yup.object({
     location: Yup.object().required(),
 });
 
-type TEvent = Yup.InferType<typeof eventDAO>;
+type TEvent = Yup.InferType<typeof eventDAO>; // use infertype to get type from yup schema
 
 // change category to ObjectId for foreign key reference from category collection
-export interface IEvent extends Omit<TEvent, "category"> {
+export interface IEvent extends Omit<TEvent, "category" | "createdBy"> {
     category: ObjectId;
+    createdBy: ObjectId;
 }
+
+//schema for event
+
+const EventSchema = new Schema<IEvent>({
+    name: {
+        type: Schema.Types.String,
+        required: true,
+    },
+    startDate: {
+        type: Schema.Types.String,
+        required: true,
+    },
+    endDate: {
+        type: Schema.Types.String,
+        required: true,
+    },
+    description: {
+        type: Schema.Types.String,
+        required: true,
+    },
+    banner: {
+        type: Schema.Types.String,
+        required: true,
+    },
+    isFeatured: {
+        type: Schema.Types.Boolean,
+        required: true,
+    },
+    isOnline: {
+        type: Schema.Types.Boolean,
+        required: true,
+    },
+    isPublish: {
+        type: Schema.Types.Boolean,
+        default: false,
+    },
+    category: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: "Category",
+    }, 
+    slug: {
+        type: Schema.Types.String,
+        unique: true,
+    },
+    createdBy: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: "User",
+    },
+    location: {
+        type: {
+            region: {
+                type: Schema.Types.String,
+            },
+            coordinates: {
+                type: [Schema.Types.Number], // [longitude, latitude]
+                default: [0, 0],
+            }
+        }
+    }
+}, {
+    timestamps: true
+});
+
+EventSchema.pre("save", function (){
+    if (!this.slug) {
+        const slug = this.name.split(" ").join("-").toLowerCase();
+        this.slug = `${slug}`;
+    }
+});
+
+export const EventModel = mongoose.model("Event", EventSchema);
